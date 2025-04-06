f28753ddb6228c3a90b7b68eb97d1a47d5d723c3
Nirali276 <niralibhavsar68@gmail.com>
2025-04-06 15:23:35 +0530

    Update 7.2.a.c

--- Practical-7/7.2.a.c
+++ Practical-7/7.2.a.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #define PI 3.14159
 
-// Function prototype
 void areaOfCircle();
 
 int main() {
     areaOfCircle(); 
     return 0;
 }
-
-// Function definition
 void areaOfCircle() {
     float r, area;
     printf("Enter the radius of the circle: ");
