a59a581fcdbd96250849f6af9b656a2e51a6c33f
Nirali276 <niralibhavsar68@gmail.com>
2025-04-15 21:43:55 +0530

    Create 8.1.c

---
+++ Practical-8/8.1.c
@@ -0,0 +1,12 @@
+//8.1.a//
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[]="programming";
+    printf("%s\n",str);
+    int length = 0;
+    length=strlen(str);
+    printf("Length of string str is %d", length);
+
+    return 0;
+}
