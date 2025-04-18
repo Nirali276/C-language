c6565f0150500edaeb15191e6d6359b7ef28cf4d
Nirali276 <niralibhavsar68@gmail.com>
2025-04-18 19:35:48 +0530

    Update 9.4.c

--- Practical-9/9.4.c
+++ Practical-9/9.4.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main(){
+    int a;
+    int *p;
+    p=&a;
+    printf("Address of variable=%d\n",p);
+    printf("Address of variable=%x\n",p);
+    printf("Address of variable=%u\n",p);
+    return 0;
+}
