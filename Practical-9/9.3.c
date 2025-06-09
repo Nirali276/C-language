c46489c60d287954bc037a7dfa2bba94a76b0e36
Nirali276 <niralibhavsar68@gmail.com>
2025-06-09 09:40:47 +0530

    Update 9.3.c

--- Practical-9/9.3.c
+++ Practical-9/9.3.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <string.h>
 
+union StudentData {
+    char name[50];
+    char subject[50];
+    float percentage;
+};
+
+struct Student {
+    char name[50];
+    char subject[50];
+    float percentage;
+};
+
+int main() {
+    struct Student s[3];  
+    int i;
+
+    for (i = 0; i < 3; i++) {
+        printf("\nEnter details for Student %d:\n", i + 1);
+
+        printf("Enter Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Enter Subject Name: ");
+        scanf("%s", s[i].subject);
+
+        printf("Enter Percentage: ");
+        scanf("%f", &s[i].percentage);
+    }
+
+    printf("\n--- Student Details ---\n");
+    for (i = 0; i < 3; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Name: %s\n", s[i].name);
+        printf("Subject: %s\n", s[i].subject);
+        printf("Percentage: %.2f%%\n", s[i].percentage);
+    }
+
+    return 0;
+}
